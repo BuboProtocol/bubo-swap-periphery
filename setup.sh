4a1a14f4b099494d1a3a1bacbd56cafc19cff5a4
Bubo Dev <dev@bubo.io>
2021-01-14 16:22:48 -0600

    Added the contract build and verification file

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Deploy contracts
+truffle migrate --reset --network bsc_testnet
+
+# Verify Contracts on Etherscan
+truffle run verify PancakeRouter01 --network bsc_testnet --license SPDX-License-Identifier
+
+# Flats Contracts
+mkdir -p flats
+rm -rf flats/*
+./node_modules/.bin/truffle-flattener contracts/PancakeRouter01.sol > flats/PancakeRouter01.sol
+
+
+
